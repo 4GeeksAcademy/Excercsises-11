ab4e078d628cde0559200f1b925f12dc553488e4
XxDrancerxX <idmdiazmora98@gmail.com>
2025-09-19 20:24:06 +0000

    a

--- src/api/excercises.py
+++ src/api/excercises.py
@@ -2778,6 +2778,91 @@
 
 #     return max_arr    
         
+#------------------------------------------------------------------------------------------------------------------------------
+#2)
+# def max_sub_arr(nums):
+#     if not nums:
+#         return 0
+
+#     curr_sum = 0
+#     max_sum = nums[0]
+
+#     for n in nums:
+#         if curr_sum < 0:
+#             curr_sum = 0
+#         curr_sum += n
+#         max_sum = max(max_sum,curr_sum)
+
+#     return max_sum    
+
+#===============================================================================================================================
+#   House Robber
+
+# Solution
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+# Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+# Total amount you can rob = 1 + 3 = 4.
+# Example 2:
+
+# Input: nums = [2,7,9,3,1]
+# Output: 12
+# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+# Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 100
+#------------------------------------------------------------------------------------------------------------------------------
+#1)
+def better_combination(money):  
+
+    prev1 = 0  #up to i-1
+    prev2= 0   #up to i-2
+
+    for x in money:
+        take = x + prev2 
+        skip = prev1 
+        curr = max(skip,take) 
+
+        prev2 = prev1
+        prev1 = curr
+
+    return prev1        
+
+input = [2, 7, 9, 3, 1]
+result = better_combination(input)
+print(result)
+
+#------------------------------------------------------------------------------------------------------------------------------
+#2)Recursion:
+def rob(nums):
+    n = len(nums)
+    memo = {}
+
+    def f(i):
+        if i >= n: return 0
+
+        if i in memo:
+            return memo[i]
+        
+        skip = f(i + 1)
+        take = nums[i] + f(i + 2)
+        memo[i] = max(skip,take)
+        return memo[i]
+    
+    return f(0)
 
 
-
+input = [2, 7, 9, 3, 1]
+result = rob(input)
+print(result)
